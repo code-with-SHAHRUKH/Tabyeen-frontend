f71092693ffee6541a7e2b2b3f500db9154ad097
code-with-SHAHRUKH <gulukwl123@gmail.com>
2025-05-19 05:58:05 +0500

    add eslint rule

--- src/app/(home)/components/AlphabetDropdown.tsx
+++ src/app/(home)/components/AlphabetDropdown.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 'use client';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
